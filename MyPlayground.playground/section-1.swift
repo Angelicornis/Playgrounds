fb1e57bcf07e4dfc466faad50ace3671ff6c1099
Tom Kumschier <dev.angelicornis@icloud.com>
2015-02-23 21:02:27 +0100

    geteilt hinzugefügt

--- MyPlayground.playground/section-1.swift
+++ MyPlayground.playground/section-1.swift
@@ -1,8 +1,75 @@
 
+func ArrayWertAusgabe<T>(vonWelchemArray ausgewähltesArray: [T], index: Int) ->T? {
+    var ergebnis: T?
+    for (key, value) in enumerate(ausgewähltesArray) {
+        if key == index {
+            ergebnis = value
+        }
+    }
+    return ergebnis
+}
 
-var ab = ["2"]
+var autoren = ["Ralf", "Stefan", "Peter", "Jürgen"]
+var autoren1 = [1, 2, 3, 4]
 
-for 
+
+ArrayWertAusgabe(vonWelchemArray: autoren1, 2)
 
 
-println(ab)
+
+
+extension Double {
+    var km: Double { return self * 1_000.0 }
+    var m: Double { return self }
+    var cm: Double { return self / 100.0 }
+    var mm: Double { return self / 1_000.0 }
+    var ft: Double { return self / 3.28084 }
+}
+let oneInch = 25.4.mm
+println("One inch is \(oneInch) meters")
+// prints "One inch is 0.0254 meters"
+let threeFeet = 3.ft
+println("Three feet is \(threeFeet) meters")
+// prints "Three feet is 0.914399970739201 meters"
+
+
+
+
+extension Array {
+    func ArrayWertAusgabe<T>(vonWelchemArray ausgewähltesArray: [T], index: Int) ->T? {
+        var ergebnis: T?
+        for (key, value) in enumerate(ausgewähltesArray) {
+            if key == index {
+                ergebnis = value
+            }
+        }
+        return ergebnis
+    }
+    
+    var ausgabeEinesValues: T? {
+        var ergebnis: T?
+        for (key, value) in enumerate(self) {
+            if key == key {
+                ergebnis = value
+            }
+        }
+        return ergebnis
+
+    }
+    
+    // Ausgabe der eingabe
+    var versuch: Array {return self}
+    
+}
+
+autoren.ArrayWertAusgabe(vonWelchemArray: autoren, index: 1)
+autoren.ausgabeEinesValues
+
+
+
+
+
+
+
+
+
--- Protokoll Kopie.playground/section-1.swift
+++ Protokoll Kopie.playground/section-1.swift
@@ -3,22 +3,46 @@
 import Cocoa
 import Foundation
 
-let berechnungsString = "12+34*2+562"
+let berechnungsString = "12+34*2+56:2"
 func parser(#stringZuParsen: String, mitWelchemSeparator separator: String, berechne:Bool = false) ->[String] {
-    var stringZuParsenGeparst =  stringZuParsen.componentsSeparatedByString(separator)
-    
+    var stringZuParsenGeparst = stringZuParsen.componentsSeparatedByString(separator)
     if berechne {
         var temp = 0
-        var ergebnis = 1
-        for wert in stringZuParsenGeparst {
-            var wertInt = wert.toInt()
-            ergebnis *= wertInt!
+        var ergebnis: Int?
+        var wert = ArrayWertAusgabe(vonWelchemArray: stringZuParsenGeparst, 0)
+        //var wertInt = wert!.toInt()
+        for wertInt in stringZuParsenGeparst {
+            switch separator {
+            case "*":
+                if ergebnis == nil {
+                    ergebnis = wert!.toInt()!
+                } else {
+                    ergebnis! *= wertInt.toInt()!
+                }
+            case ":":
+                if ergebnis == nil {
+                    ergebnis = wert!.toInt()!
+                } else {
+                    ergebnis! /= wertInt.toInt()!
+                }
+            default: ergebnis! = 0
+            }
         }
-        return ["\(ergebnis)"]
+        return ["\(ergebnis!)"]
     } else {
         return stringZuParsenGeparst
     }
 }
+func ArrayWertAusgabe<T>(vonWelchemArray ausgewähltesArray: [T], index: Int) ->T? {
+    var ergebnis: T?
+    for (key, value) in enumerate(ausgewähltesArray) {
+        if key == index {
+            ergebnis = value
+        }
+    }
+    return ergebnis
+}
+
 
 var arrayOfWords = parser(stringZuParsen: berechnungsString, mitWelchemSeparator: "+")
 println("Ergebniss1: \n \(arrayOfWords)\n")
@@ -27,16 +51,31 @@ println("Ergebniss1: \n \(arrayOfWords)\n")
 var arrayOfWords2 = [String]()
 var temparray = [Int]()
 for (index1, wort1) in enumerate(arrayOfWords) {
-    if wort1.toInt() == nil {
-        println ("String vor dem zweiten Parsen \n \(wort1)\n")
-        arrayOfWords2 = (parser(stringZuParsen: wort1, mitWelchemSeparator: "*", berechne: true))
-        temparray.append(index1)
-        for i in arrayOfWords2 {
-            arrayOfWords.removeAtIndex(index1)
-            arrayOfWords.insert(i, atIndex: index1)
+    for c in wort1 {
+        if c == "*" {
+            println ("String vor dem zweiten Parsen * \n \(wort1)\n")
+            arrayOfWords2 = (parser(stringZuParsen: wort1, mitWelchemSeparator: "*", berechne: true))
+            temparray.append(index1)
+            for i in arrayOfWords2 {
+                arrayOfWords.removeAtIndex(index1)
+                arrayOfWords.insert(i, atIndex: index1)
+            }
+        } else if c == ":" {
+            println ("String vor dem zweiten Parsen : \n \(wort1)\n")
+            arrayOfWords2 = (parser(stringZuParsen: wort1, mitWelchemSeparator: ":", berechne: true))
+            temparray.append(index1)
+            for i in arrayOfWords2 {
+                arrayOfWords.removeAtIndex(index1)
+                arrayOfWords.insert(i, atIndex: index1)
+            }
         }
     }
 }
 println("Ergebniss2: \n \(arrayOfWords)\n")
 
 println("Das letzte Vorergebnis Lautet: \n \(arrayOfWords)\n")
+var ergebnis = 0
+for i in arrayOfWords {
+    ergebnis += i.toInt()!
+}
+println("Das Ergebnis Lautet: \n \(ergebnis)\n")
